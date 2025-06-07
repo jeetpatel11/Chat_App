a645c46f1bcf73da97141716998b8ea3b21dab58
Jeet patel <143858930+jeetpatel11@users.noreply.github.com>
2025-06-07 15:07:20 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,17 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173, // Default port for development
+    host: true
+  },
+  preview: {
+    port: process.env.PORT || 5173,
+    host: '0.0.0.0'
   },
   build: {
     outDir: 'dist', // Output directory for build
+    sourcemap: false
   },
+  css: {
+    postcss: './postcss.config.js'
+  }
 });
