6faacd3280c25892d7cbef6dc5e6b186e5fbb45b
jeetpatel11 <pateljeetr5012@gmail.com>
2026-01-02 16:30:30 +0530

    levo remove

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE=== "development" ? "http://localhost:5001/api":"/api",
+  baseURL: "http://13.205.7.6:5001/api",  // <--- add your backend IP and port
   headers: {
     'Content-Type': 'application/json',
   },
   withCredentials: true,
 });
-
